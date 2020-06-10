8d6f96ccfa88ff5a52ece75e5a4c07ec62622a21
Gi Pyo Kim <gipyok@gmail.com>
2020-06-09 21:29:00 -0700

    hour glass solution

---
+++ Hourglass.swift
@@ -0,0 +1,16 @@
+func hourglassSum(arr: [[Int]]) -> Int {
+    var highest: Int = Int.min
+    for i in 0..<arr[0].count-2 {
+        var sum: Int
+        for j in 0..<arr[0].count-2 {
+            sum = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+            sum = sum + arr[i+1][j+1]
+            sum = sum + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+
+            if sum > highest {
+                highest = sum
+            }
+        }
+    }
+    return highest
+}
