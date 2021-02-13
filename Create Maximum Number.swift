30bbe2383b3dbda32d25abe2acf712f9f9710a3c
Gi Pyo Kim <gipyok@gmail.com>
2021-02-12 20:16:11 -0800

    Create Maximum Number swift solution. Went different approach.

--- Create Maximum Number.swift
+++ Create Maximum Number.swift
@@ -1,132 +1,77 @@
 class Solution {
     func maxNumber(_ nums1: [Int], _ nums2: [Int], _ k: Int) -> [Int] {
-        var n1 = nums1
-        var n2 = nums2
-        var saved1: Int?
-        var saved2: Int?
-        var saved: Bool = false
-        var saveCounter = 0
-        var output: [Int] = []
-
-        while output.count < k {
-            
-            if saved == true {
-                saveCounter += 1
-                if saveCounter == 2 {
-                    if let save = saved1 {
-                        var index = n1.firstIndex(of: -1)!
-                        n1[index] = save
-                        print("n1: \(n1)")
-                    }
-                    if let save = saved2 {
-                        var index = n2.firstIndex(of: -1)!
-                        n2[index] = save
-                        print("n2: \(n2)")
-                    }
-                    saved = false
-                    saveCounter = 0
-                }
-            }
-            
-            if output.count + n1.count + n2.count == k {
-                var n1IsGreater = false
-                var index: Int = 0
-                while true {
-                    if n1.count <= index { n1IsGreater = false; break }
-                    if n2.count <= index { n1IsGreater = true; break }
-                    
-                    if n1[index] > n2[index] {
-                        n1IsGreater = true
-                        break
-                    } else if n2[index] > n1[index] {
-                        n1IsGreater = false
-                        break
+        
+        func isArr(_ arr1: [Int], greaterThan arr2: [Int]) -> Bool {
+            var a1 = arr1
+            var a2 = arr2
+            while !a1.isEmpty || !a2.isEmpty {
+                if a1.count > 0 && a2.count > 0 {
+                    if a1[0] > a2[0] {
+                        return true
+                    } else if a2[0] > a1[0] {
+                        return false
                     } else {
-                        index += 1
+                        a1 = Array(a1[1...])
+                        a2 = Array(a2[1...])
                     }
-                }
-                
-                output += n1IsGreater ? n1 + n2 : n2 + n1
-                
-            } else {
-                var max1: Int = -1
-                var max2: Int = -1
-                if let m1 = n1.max() {
-                    max1 = m1
-                }
-                if let m2 = n2.max() {
-                    max2 = m2
-                }
-            
-                if max1 > max2 {
-                    saved1 = checkRemaindingValuesFor(value: max1, forArr: &n1, arr: n2, output: output, k: k)
-                    if saved1 != nil { saved = true; continue }
-                    sliceArray(from: max1, arr: &n1)
-                    output.append(max1)
-
-                } else if max2 > max1 {
-                    saved2 = checkRemaindingValuesFor(value: max2, forArr: &n2, arr: n1, output: output, k: k)
-                    if saved2 != nil { saved = true; continue }
-                    sliceArray(from: max2, arr: &n2)
-                    output.append(max2)
-
+                } else if a1.count > 0 {
+                    return true
                 } else {
-                    let index1 = n1.firstIndex(of: max1)!
-                    let max1Sub = Array(n1[..<index1]).max()
-                    
-                    let index2 = n2.firstIndex(of: max2)!
-                    let max2Sub = Array(n2[..<index2]).max()
-                    
-                    if let m1Sub = max1Sub, let m2Sub = max2Sub {
-                        if m1Sub > m2Sub {
-                            saved2 = checkRemaindingValuesFor(value: max2, forArr: &n2, arr: n1, output: output, k: k)
-                            if saved2 != nil { saved = true; continue }
-                            sliceArray(from: max2, arr: &n2)
-                            output.append(max2)
-                        } else {
-                            saved1 = checkRemaindingValuesFor(value: max1, forArr: &n1, arr: n2, output: output, k: k)
-                            if saved1 != nil { saved = true; continue }
-                            sliceArray(from: max1, arr: &n1)
-                            output.append(max1)
-                        }
-                    } else if let m1Sub = max1Sub {
-                        saved2 = checkRemaindingValuesFor(value: max2, forArr: &n2, arr: n1, output: output, k: k)
-                        if saved2 != nil { saved = true; continue }
-                        sliceArray(from: max2, arr: &n2)
-                        output.append(max2)
-                    } else {
-                        saved1 = checkRemaindingValuesFor(value: max1, forArr: &n1, arr: n2, output: output, k: k)
-                        if saved1 != nil { saved = true; continue }
-                        sliceArray(from: max1, arr: &n1)
-                        output.append(max1)
-                    }
+                    return false
                 }
             }
-            
+            return false
         }
         
-        return output
-    }
-    
-    func sliceArray(from value: Int, arr: inout [Int]) {
-        var index = arr.firstIndex(of: value)!
-        index = arr.index(index, offsetBy: 1)
-        arr = Array(arr[index...])
-    }
+        func getMax(_ arr1: [Int], _ arr2: [Int]) -> [Int] {
+            if isArr(arr1, greaterThan: arr2) {
+                return arr1
+            } else {
+                return arr2
+            }
+        }
+        
+        func merge(_ n1: inout [Int], _ n2: inout [Int]) -> [Int] {
+            var res = [Int]()
+            while !n1.isEmpty || !n2.isEmpty {
+                if isArr(n1, greaterThan: n2) {
+                    res.append(n1[0])
+                    n1 = Array(n1[1...])
+                } else {
+                    res.append(n2[0])
+                    n2 = Array(n2[1...])
+                }
+            }
+            return res
+        }
+        
+        func findMax(_ nums: [Int], _ length: Int) -> [Int] {
+            var l = [Int]()
+            var maxpop = nums.count - length
+            for i in 0..<nums.count {
+                while maxpop > 0, l.count > 0, nums[i] > l[l.index(before: l.endIndex)] {
+                    l.removeLast()
+                    maxpop -= 1
+                }
+                l.append(nums[i])
+            }
+            return Array(l[..<length])
+        }
+        
+        let n1 = nums1.count
+        let n2 = nums2.count
+        var res = Array(repeating: 0, count: k)
+        
+        for i in 0..<k+1 {
+            let j = k-i
+            if i > n1 || j > n2 { continue }
+            var l1 = findMax(nums1, i)
+            var l2 = findMax(nums2, j)
+            res = getMax(res, merge(&l1, &l2))
+            print(res)
+        }
+        
+        return res
 
-    func checkRemaindingValuesFor(value: Int, forArr: inout [Int], arr: [Int], output: [Int], k: Int) -> Int? {
-        var index = forArr.firstIndex(of: value)!
-        index = forArr.index(index, offsetBy: 1)
-        let newArr = Array(forArr[index...])
-        
-        if output.count + newArr.count + arr.count + 1 < k {
-            var maxIndex = forArr.firstIndex(of: value)!
-            forArr[maxIndex] = -1
-            print(forArr)
-            return value
-        } else {
-            return nil
-        }
     }
 }
-
